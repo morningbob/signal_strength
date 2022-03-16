ac5e3f78d5f9abca9d84f4e8544d12e0a63f4828
Jess on the Hope <jesshon.py326@gmail.com>
2022-03-16 15:29:27 -0400

    initial commit
    created ios app skeleton for the signal strength project

---
+++ signal_strength_ios/signal_strength_ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  signal_strength_ios
+//
+//  Created by Jessie Hon on 2022-03-16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
